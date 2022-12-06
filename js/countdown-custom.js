d15bd8e00c6c7d3105907b2e7e1397d3b1118df4
KentHinaloc <119943638+KentHinaloc@users.noreply.github.com>
2022-12-07 01:29:59 +0800

    1st HTML Reorganization and Modifications

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2021, 03, 20, 8)}); // year, month, date, hour
+        });
+});		
+
